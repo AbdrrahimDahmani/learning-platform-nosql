2aa310529e4f682f5497d431c06362f589449169
Abderrahim Dahmani <abdrrahim.dahmani.2002@gmail.com>
2025-01-10 14:58:38 +0100

    updated findCourseStat function

--- src/services/mongoService.js
+++ src/services/mongoService.js
@@ -28,21 +28,39 @@ async function create(collectionName, course) {
 }
 
 // Fonction pour trouver les statistiques d'un cours
-async function findCourseStats(collectionName, courseName) {
+async function findCourseStats(collectionName) {
   try {
     const db = getDbObject();
-    let collection = db.collection(collectionName);
-    let course = await collection.findOne({ name: courseName });
-    if (!course) {
-      throw new Error("Course not found");
+    const collection = db.collection(collectionName);
+
+    //  calculation du statistics total en utilisant l'aggregation
+    const stats = await collection
+      .aggregate([
+        {
+          $group: {
+            _id: null, // Group all
+            totalCourses: { $sum: 1 }, //  total des cours
+            totalDuration: { $sum: "$duration" }, // Somme des durations
+            totalStudentsEnrolled: { $sum: "$studentsEnrolled" }, // Somme des etudiants inscrits
+          },
+        },
+      ])
+      .toArray();
+
+    if (stats.length === 0) {
+      throw new Error("No courses found");
     }
+
+    // Format the results
     const courseStats = {
-      Duration: course.duration,
-      StudentsEnroled: course.studentsEnroled,
+      totalCourses: stats[0].totalCourses,
+      totalDuration: stats[0].totalDuration,
+      totalStudentsEnrolled: stats[0].totalStudentsEnrolled,
     };
+
     return courseStats;
   } catch (error) {
-    console.error("Error : ", error);
+    console.error("Error retrieving course stats:", error);
     throw error;
   }
 }
